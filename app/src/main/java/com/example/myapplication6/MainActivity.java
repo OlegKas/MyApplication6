951fd6fe0f1a80569d230a09551ab558b41add35
olleg <oolegkaastkin@yandex.ru>
2024-04-24 19:36:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication6/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.myapplication6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "my_channel_id";
+    private static final String CHANNEL_NAME = "My Channel";
+    private static final int NOTIFICATION_ID = 1;
+    private static final int OVERLAY_PERMISSION_REQUEST_CODE = 1234;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button butt = findViewById(R.id.butt);
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startBannerService();
+            }
+        });
+    }
+    private void startBannerService()
+    {
+        Intent intent = new Intent(this,BannerService.class);
+        startService(intent);
+    }
+
+    private void createNotificationChannel()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            CharSequence n = CHANNEL_NAME;
+            String d = "Channel Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,n,importance);
+            channel.setDescription(d);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+    }
+
+    private void showNotification()
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Уведомление")
+                .setContentText("Содержание уведомления")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
+    }
+}
